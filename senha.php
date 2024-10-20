7f273c389c190cffa41627796931ced74053af73
TioDuPets <tiodupets@gmail.com>
2024-10-20 09:56:22 -0300

    Delete senha.php

--- senha.php
+++
@@ -1,8 +0,0 @@
-<?php
-$senha = "1234";
-// não usar pois é fácil de descobrir $criptografada = md5($senha);
-$hash = password_hash($senha, PASSWORD_DEFAULT);
-//$sql = "SELECT * FROM usuarios WHERE senha = '$criptografada'";
-echo $hash;
-//echo password_verify($senha, '$2y$10$Dh/NBSgzvjnzyGCLOP8tmeue0YLgxTnZetjN6tFXfN3XBgjJbPynq');
-?>
